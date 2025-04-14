d3a1ca8cfb8f3a2027f5315993448507514858cf
Tushar57517 <sainitushar575@gmail.com>
2025-04-14 12:08:25 +0530

    initial commit

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+class HomeView(APIView):
+    def get(self, request):
+        return Response({"message":"Hello, World!"}, status=status.HTTP_200_OK)
